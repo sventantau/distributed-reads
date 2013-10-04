a04ded863098e3c0b375ec78325263ddf15c5c50
Clint Shryock <clint@ctshryock.com>
2013-10-04 11:49:09 -0500

    User person's connection for sleep test thing:

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,7 @@ class Person < ActiveRecord::Base
   replicated_model
   def sleeper
     # we sleep for 2 so something shows up in the slow query logs
-    ActiveRecord::Base.connection.execute("select pg_sleep(2);")
+    Person.connection.execute("select pg_sleep(2);")
     "name read from follower: #{name}"
   end
 end
