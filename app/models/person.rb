15256cfe40aff61ccb5ad49545adff49251174b9
Clint Shryock <clint@ctshryock.com>
2013-10-04 11:30:34 -0500

    Sleep different, etc

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,7 @@
 class Person < ActiveRecord::Base
   def sleeper
+    # we sleep for 2 so something shows up in the slow query logs
     ActiveRecord::Base.connection.execute("select pg_sleep(2);")
-    "thing!"
+    "name read from follower: #{name}"
   end
 end
