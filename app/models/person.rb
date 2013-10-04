5edd23d6fbcc9da8e9eb9ebd97cbb0746704207b
Clint Shryock <clint@ctshryock.com>
2013-10-04 11:37:41 -0500

    Set person as a replicated model, to send reads to the follower

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ActiveRecord::Base
+  replicated_model
   def sleeper
     # we sleep for 2 so something shows up in the slow query logs
     ActiveRecord::Base.connection.execute("select pg_sleep(2);")
