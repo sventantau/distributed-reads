ac86b01991c1b300938949d7f3a4fd2a73a83ea2
Clint Shryock <clint@ctshryock.com>
2014-03-13 11:06:04 -0500

    Remove test sleep() call

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,8 +1,3 @@
 class Person < ActiveRecord::Base
   replicated_model
-  def sleeper
-    # we sleep for 2 so something shows up in the slow query logs
-    Person.connection.execute("select pg_sleep(3);")
-    "name read from follower: #{name}"
-  end
 end
