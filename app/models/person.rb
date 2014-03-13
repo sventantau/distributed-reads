7071de4370f3a1de970b7498820c64d5a3b2df20
Clint Shryock <clint@ctshryock.com>
2014-03-13 11:02:06 -0500

    Bump versions

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,7 @@ class Person < ActiveRecord::Base
   replicated_model
   def sleeper
     # we sleep for 2 so something shows up in the slow query logs
-    Person.connection.execute("select pg_sleep(2);")
+    Person.connection.execute("select pg_sleep(3);")
     "name read from follower: #{name}"
   end
 end
